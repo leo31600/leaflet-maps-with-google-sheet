11c53a8f266692cbf71a42b879b83bac3299246b
leo31600 <78046500+leo31600@users.noreply.github.com>
2021-01-26 23:04:50 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1tg68hmSzRHaQyeO53BQOX21qC3IQ2Js3H8vKbGhJOQ8/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyCQsGGPDl14Tn34-1CML-AAbA_UM329D64';
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
